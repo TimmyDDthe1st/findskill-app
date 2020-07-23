ec908c5cb41120ffefce2f99a0e14572e6ba4b8d
Tim Douglas <me@timdouglas.net>
2020-07-23 08:53:46 +0100

    Changed to mono repo

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import MapDisplay from './MapDisplay';
+import location from '../fixtures/locationData.json';
+
 import '../styles/App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Skill App</h1>
+      <MapDisplay location={location} zoomLevel={8}/>
     </div>
   );
 }
