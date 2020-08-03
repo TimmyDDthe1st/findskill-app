8c1eb75db741d15977be1dd04a195cb0378a50eb
Tim Douglas <me@timdouglas.net>
2020-08-03 19:58:13 +0100

    added fields and ready to do a post request to end point

--- src/components/AddSkillForm.js
+++ src/components/AddSkillForm.js
@@ -1,14 +1,19 @@
+/* eslint-disable no-useless-escape */
 import React, { useState } from 'react'
 import { Input, 
          Button, 
          Checkbox, 
          Form,
+         TextArea,
 } from 'semantic-ui-react';
 
 const AddSkillForm = () => {
-  const [nameText, setNameText] = useState();
-  const [skillText, setSkillText] = useState();
-  const [postcodeText, setPostcodeText] = useState();
+  const [nameText, setNameText] = useState('');
+  const [skillText, setSkillText] = useState('');
+  const [postcodeText, setPostcodeText] = useState('');
+  const [emailText, setEmailText] = useState('');
+  const [freeBool, setFreeBool] = useState(false);
+  const [professionalBool, setProfessionalBool] = useState(false);
   const postcodeRegex = '^(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$'
 
 
@@ -18,7 +23,6 @@ const AddSkillForm = () => {
 
   const handleSkillInput = (e) => {
     setSkillText(e.target.value);
-
   }
 
   const handlePostcodeInput = (e) => {
@@ -29,9 +33,27 @@ const AddSkillForm = () => {
     } else {
       console.log('set loading from semantic-ui on each type');
     }
-
     setPostcodeText(uppcasedInput);
+  }
 
+  const handleEmailInput = (e) => {   
+    const userInput = e.target.value;
+
+    setEmailText(userInput);
+  }
+
+  const handleFreeCheckbox = (e) => {
+    setFreeBool(e.target.checked);
+  }
+
+  const handleProfessionalCheckbox = (e) => {
+    setProfessionalBool(e.target.checked);
+  }
+
+  const handleFormSubmit = () => {
+    const requestParams = [nameText, skillText, postcodeText, emailText, freeBool, professionalBool];
+
+    console.log(requestParams);
   }
 
   return <Form>
@@ -52,7 +74,24 @@ const AddSkillForm = () => {
       />
     </Form.Field>
     <Form.Field>
-      <Checkbox label='Professional' />
+      <label>Description:</label>
+      <br></br>
+      <TextArea 
+        placeholder="Description..."
+        name="description"
+      />
+    </Form.Field>
+    <Form.Field>
+      <Checkbox 
+        label='Professional'
+        onClick={handleProfessionalCheckbox} 
+      />
+    </Form.Field>
+    <Form.Field>
+      <Checkbox 
+        label='Free' 
+        onClick={handleFreeCheckbox}
+      />
     </Form.Field>
     <Form.Field>
       <label>Postcode:</label>
@@ -62,7 +101,15 @@ const AddSkillForm = () => {
         onChange={handlePostcodeInput}
       />
     </Form.Field>
-    <Button type='submit'>Submit</Button>
+    <Form.Field>
+      <label>Email:</label>
+      <Input 
+        placeholder="Email..."
+        name="email"
+        onChange={handleEmailInput}
+      />
+    </Form.Field>
+    <Button onClick={handleFormSubmit}>Add Skill</Button>
   </Form>
 }
 
